cfb18aa2db235a6b72fc7c072ad5611e1f2f8072
Xiaojun Dong <akatsuki6725@gmail.com>
2022-11-26 14:12:12 -0800

    feat: Add timer to test framework

--- test_framework.cpp
+++ test_framework.cpp
@@ -1,4 +1,5 @@
 #include <parlay/delayed_sequence.h>
+#include <parlay/internal/get_time.h>
 #include <parlay/primitives.h>
 #include <parlay/sequence.h>
 #include <parlay/utilities.h>
@@ -30,12 +31,14 @@ int main(int argc, char* argv[]) {
   size_t n = 1000000;
   size_t k = 1000;
   size_t alpha = n;
+  size_t num_rounds = 5;
   if (argc == 1) {
     printf(
-        "Usage: ./edit_distance <n> <k> <alpha>\n"
+        "Usage: ./edit_distance <n> <k> <alpha> <rounds>\n"
         "n: length of strings\n"
         "k: estimated number of edits\n"
-        "alpha: alphabet size\n");
+        "alpha: alphabet size\n"
+        "rounsd: number of rounds");
     exit(0);
   }
   if (argc >= 2) {
@@ -49,7 +52,23 @@ int main(int argc, char* argv[]) {
   }
   parlay::sequence<Type> A, B;
   std::tie(A, B) = generate_strings(n, k, alpha);
-  // size_t edits = edit_distance(A, B);
-  // printf("#edits: %zu\n", edits);
+
+  double total_time;
+  for (size_t i = 0; i <= num_rounds; i++) {
+    parlay::internal::timer t;
+    if (i == 0) {
+      // size_t edits = edit_distance(A, B);
+      // printf("#edits: %zu\n", edits);
+      // }
+      t.stop();
+      printf("Warmup round: %f\n", t.total_time());
+    } else {
+      // edit_distance(A, B);
+      t.stop();
+      printf("Round %zu: %f\n", i, t.total_time());
+      total_time += t.total_time();
+    }
+  }
+  printf("Average time: %f\n", total_time / num_rounds);
   return 0;
 }

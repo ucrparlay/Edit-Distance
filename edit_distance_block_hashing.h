f88e1672561c1a37cd130a3e99d20feae8ffbffd
alanSquirrelyz <yliu908@ucr.edu>
2023-01-20 15:53:37 -0800

    fix: speedup

--- edit_distance_block_hashing.h
+++ edit_distance_block_hashing.h
@@ -5,19 +5,29 @@
 template <typename Seq>
 int EditDistanceBlockHashParallel(const Seq &a, const Seq &b) {
   // build sparse table
+
+  /**
+   * rocord of building time
+   */
+  Timer tmr;
+
   int n = a.size();
   int m = b.size();
   if (n == 0) return m;
   if (m == 0) return n;
-  vector<vector<int>> table_A;
-  vector<vector<int>> table_B;
-  vector<int> S_A;
-  vector<int> S_B;
-  vector<int> aux_table;
+  parlay::sequence<parlay::sequence<int>> table_A;
+  parlay::sequence<parlay::sequence<int>> table_B;
+  parlay::sequence<int> S_A;
+  parlay::sequence<int> S_B;
+  parlay::sequence<int> aux_table;
   int blk_size = construct_table(a, b, table_A, table_B, S_A, S_B, aux_table,
                                  std::min(n, m));
   auto Diag = [&](int i, int j) { return i - j + m; };
   parlay::sequence<int> max_row(n + m + 1, -1), temp(n + m + 1);
+
+  double building_tm = tmr.elapsed();
+  std::cout << " building time of block: " << building_tm << std::endl;
+
   max_row[Diag(0, 0)] = block_query_lcp(0, 0, a, b, table_A, table_B, S_A, S_B,
                                         aux_table, blk_size);
 
